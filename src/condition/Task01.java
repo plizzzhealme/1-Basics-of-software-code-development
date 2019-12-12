ad198451f3c20d616dd1db37019cff395f059215
Den <plizzz.healme@gmail.com>
2019-12-12 14:24:45 +0300

    clean up code

--- src/condition/Task01.java
+++ src/condition/Task01.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 class Task01 {
     private static final int TRIANGLE_SUM = 180;
     private static final int RIGHT_ANGLE = 90;
-    private static final String INPUT_MSG = "Input the angles A and B:";
+    private static final String INPUT_MSG = "Enter the angles A and B:";
     private static final String TRIANGLE_MSG = "A triangle with angles A=%d and B=%d exists. ";
     private static final String RIGHT_TRIANGLE_MSG = "It's a right triangle.";
     private static final String NOT_TRIANGLE_MSG = "A triangle with angles A=%d and B=%d doesn't exist.";
