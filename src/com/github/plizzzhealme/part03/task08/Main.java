c26f54119b91d1d5a31c8128e73c6d61c205c20b
Den <plizzz.healme@gmail.com>
2020-04-24 13:01:25 +0300

    part03 task08 refactoring

--- src/com/github/plizzzhealme/part03/task08/Main.java
+++ src/com/github/plizzzhealme/part03/task08/Main.java
@@ -14,34 +14,51 @@ class Main {
         m = getInt();
         n = getInt();
 
-        System.out.printf("Common digits for %d and %d:", m, n);
-        printCommonDigits(m, n);
+        System.out.printf("Common digits for %d and %d%n:", m, n);
+        print(m, n);
     }
 
-    private static void printCommonDigits(int m, int n) {
-        int digitsNumber = 10;
+    private static void print(int m, int n) {
+        int mTable;
+        int nTable;
+        int table;
+        int i = 0;
 
-        for (int i = 0; i < digitsNumber; i++) {
-            if (hasDigit(m, i) && hasDigit(n, i)) {
-                System.out.print(" " + i);
+        mTable = buildDigitsTable(m);
+        nTable = buildDigitsTable(n);
+        table = mTable + nTable;
+
+        while (table > 0) {
+            if (table % 10 == 2) {
+                System.out.println(i);
             }
+            table /= 10;
+            i++;
         }
     }
 
-    private static boolean hasDigit(int number, int digit) {
-        int digitsNumber = 10;
-        int temp = Math.abs(number);
+    /*
+    Builds digits table for the number. Each digit int the table is 0 or 1,
+    depending on whether the corresponding digit is found in the number or not.
+    Adds 10^0 for 0, 10^1 for 1, 10^2 for 2 and so on.
 
-        if (temp < digitsNumber) {
-            return temp == digit;
-        }
+    For example:
+    table:               0001101111 is for 6523210
+    corresponding digit: 9876543210
+     */
+    private static int buildDigitsTable(int n) {
+        int table = 0;
+        int temp = Math.abs(n);
 
-        while (temp != 0) {
-            if (temp % digitsNumber == digit) {
-                return true;
+        do {
+            int digit = temp % 10;
+            int inc = (int) Math.pow(10, digit);
+
+            if (table % (10 * inc) < inc) {
+                table += inc;
             }
             temp /= 10;
-        }
-        return false;
+        } while (temp > 0);
+        return table;
     }
 }
